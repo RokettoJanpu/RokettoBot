3716255f5af25f42b29887804b2ccebd908d638d
RokettoJanpu <rokettojanpu@gmail.com>
2019-04-01 18:55:58 -0400

    Update hug.js

--- bot_commands/hug.js
+++ bot_commands/hug.js
@@ -3,7 +3,7 @@ module.exports = {
 	description: `Give a big bear hug to other users! <3`,
 	execute(message, user, atUser, toUser, query, queryString, args, mentions){
         a = args.join(` & `);
-		botResponse = a?`:heart: ${user} passionately embraces ${a} :heart:`:
+	botResponse = a?`:heart: ${user} passionately embraces ${a} :heart:`:
 		`${user} hugs themself out of loneliness... :pensive:`;
         message.channel.send(botResponse);
         logResponse = botResponse;
