4ddc588792f5f50bd34591e6b1a89cf076fb4009
RokettoJanpu <rokettojanpu@gmail.com>
2019-04-01 18:56:38 -0400

    Update botname.js

--- bot_commands/botname.js
+++ bot_commands/botname.js
@@ -2,8 +2,8 @@ module.exports = {
 	name: `botname`,
 	description: `Say my name...`,
 	execute(message, user, atUser, toUser, query, queryString, args, mentions){
-        botResponse = `My name is RokettoBot.`;
-	message.channel.send(botResponse);
-	logResponse = botResponse;
+        	botResponse = `My name is RokettoBot.`;
+		message.channel.send(botResponse);
+		logResponse = botResponse;
 	},
 };
