fa74f38e799faca283e0e5f0cf29ab9079ac7ec5
RokettoJanpu <rokettojanpu@gmail.com>
2019-04-15 13:23:09 -0400

    Create repeat.js

---
+++ bot_commands/repeat.js
@@ -0,0 +1,8 @@
+module.exports = {
+	name: `repeat`,
+	description: `Simon says!`,
+	execute(message, user, atUser, toUser, query, queryString, args, mentions){
+        	botResponse = query||`What am I supposed to say, dumbass? ${atUser}`;
+        	logResponse = botResponse;
+	},
+};
