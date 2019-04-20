6ffe7819df7c2a02d7162a8f886a3f26d467ff9d
RokettoJanpu <rokettojanpu@gmail.com>
2019-04-20 12:57:37 -0400

    Update repeat.js

--- bot_commands/repeat.js
+++ bot_commands/repeat.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: `repeat`,
 	description: `Simon says!`,
 	execute(message, user, atUser, toUser, query, queryString, args, mentions){
-        	botResponse = query?query:`What am I supposed to say, dumbass? ${atUser}`;
-        	logResponse = botResponse;
+        botResponse = query?query:`What am I supposed to say, dumbass? ${atUser}`;
+        logResponse = botResponse;
 	},
 };
